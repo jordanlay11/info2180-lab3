8cf0e0f852025f1cd51b2dc63098b7f4ec0b7841
jordanlay11 <jordanlaylor0@gmail.com>
2025-10-16 09:38:13 -0500

    Ex 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,3 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
   squares.forEach(sq => sq.classList.add('square'));
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const status = document.getElementById('status');
+    const squares = Array.from(document.querySelectorAll('#board div'));
+
+    let board = Array(9).fill(null); // track state: 'X', 'O' or null
+    let currentPlayer = 'X';
+
+    squares.forEach((sq, idx) => {
+        sq.addEventListener('click', () => {
+            board[idx] = currentPlayer;
+            sq.textContent = currentPlayer;
+            sq.classList.add(currentPlayer);
+
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            status.textContent = `Player ${currentPlayer}'s turn.`;
+        });
+    });
+
+});
